4e36a69c4a1b0a25aca4a2f18409f0771128956c
Kaan Danisment <kaandanisment1qgmail.com>
2024-07-03 13:39:06 +0300

    movies endpoints completed

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -17,7 +17,72 @@ async function getAllMovies(req, res) {
     res.json(err);
   }
 }
+
+async function getMovie(req, res, next) {
+  try {
+    const movie = await Movie.findById({ _id: req.params.id });
+    if (!movie) {
+      next({ message: "There is no movie in this ID" });
+    } else {
+      res.json(movie);
+    }
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function updateMovie(req, res) {
+  try {
+    const movie = await Movie.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!movie) {
+      next({ message: "There is no movie in this ID" });
+    } else {
+      res.json(movie);
+    }
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function deleteMovie(req, res, next) {
+  try {
+    const movie = await Movie.findByIdAndDelete(req.params.id);
+    if (!movie) {
+      next({ message: "There is no movie in this ID" });
+    } else {
+      res.json(movie);
+    }
+  } catch (err) {
+    res.json(err);
+  }
+}
+async function top10Movies(req, res) {
+  try {
+    const movies = await Movie.find({}).limit(10).sort({ imdb_score: -1 });
+    res.json(movies);
+  } catch (err) {
+    res.json(err);
+  }
+}
+
+async function between2Years(req, res) {
+  try {
+    const movies = await Movie.find({
+      year: { $gte: req.params.start_year, $lte: req.params.end_year },
+    });
+    res.json(movies);
+  } catch (err) {
+    res.json(err);
+  }
+}
 module.exports = {
   createMovie,
   getAllMovies,
+  getMovie,
+  updateMovie,
+  deleteMovie,
+  top10Movies,
+  between2Years,
 };
--- routes/movieRoute.js
+++ routes/movieRoute.js
@@ -6,5 +6,10 @@ const router = expres.Router();
 
 router.route("/").post(movieController.createMovie);
 router.route("/").get(movieController.getAllMovies);
+router.route("/top10").get(movieController.top10Movies);
+router.route("/:id").get(movieController.getMovie);
+router.route("/:id").put(movieController.updateMovie);
+router.route("/:id").delete(movieController.deleteMovie);
+router.route("/:start_year/:end_year").get(movieController.between2Years);
 
 module.exports = router;
